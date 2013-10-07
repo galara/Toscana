bf3956eeb1d18748ecccc1438358681cca698a17
Arandi López <arandilopez.93@gmail.com>
2013-10-07 11:30:25 -0500

    Initial commit

---
+++ src/com/InitalClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author ArandiLopez
+ */
+public class InitalClass {
+    /**
+     * This is the initial class
+     */
+}
