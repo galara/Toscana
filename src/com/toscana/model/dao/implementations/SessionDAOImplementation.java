0c9829200af877026fd48d4c3365aa4d627ce8fc
Arandi López <arandilopez.93@gmail.com>
2013-10-28 20:58:04 -0600

    Create DAOS interfaces and implementations

---
+++ src/com/toscana/model/dao/implementations/SessionDAOImplementation.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toscana.model.dao.implementations;
+
+import com.toscana.model.dao.interfaces.SessionDAO;
+import com.toscana.model.sessions.Session;
+
+
+public class SessionDAOImplementation extends GenericDAOImplementation<Session, Integer> implements SessionDAO {
+
+    
+    
+}
