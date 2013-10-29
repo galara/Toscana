c0c3233008c423bb07d8659323c2f6cedc122e34
Arandi López <arandilopez.93@gmail.com>
2013-10-28 21:32:08 -0600

    Create DAOS interfaces and implementations

---
+++ src/com/toscana/model/dao/implementations/SummaryPaymentsTypeDAOImplementation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toscana.model.dao.implementations;
+
+import com.toscana.model.dao.interfaces.SummaryPaymentsTypeDAO;
+import com.toscana.model.reports.types.SummaryPaymentsType;
+
+
+public class SummaryPaymentsTypeDAOImplementation extends GenericDAOImplementation<SummaryPaymentsType, Integer> implements SummaryPaymentsTypeDAO {
+
+    
+}
---
+++ src/com/toscana/model/dao/interfaces/SummaryPaymentsTypeDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toscana.model.dao.interfaces;
+
+import com.toscana.model.reports.types.SummaryPaymentsType;
+
+/**
+ *
+ * @author Arandi
+ */
+public interface SummaryPaymentsTypeDAO extends GenericDAO<SummaryPaymentsType, Integer>{
+    
+}
