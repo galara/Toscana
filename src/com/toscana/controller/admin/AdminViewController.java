5482edfd5a4eef10563f202ef08b11034997eb33
Arandi López <arandilopez.93@gmail.com>
2013-11-28 16:28:13 -0600

    Update admin view controller

--- src/com/toscana/controller/admin/AdminViewController.java
+++ src/com/toscana/controller/admin/AdminViewController.java
@@ -4,10 +4,29 @@
  */
 package com.toscana.controller.admin;
 
+import com.toscana.dao.managers.UserDAOManager;
+
 /**
  *
  * @author Arandi
  */
 public class AdminViewController {
     
+    private UserDAOManager userDAOManager;
+
+    public AdminViewController() {
+        userDAOManager = new UserDAOManager();
+    }
+    
+    public void addUser(){
+        
+    }
+    
+    public void updateUser(){
+        
+    }
+    
+    public void deleteUser(){
+        
+    }
 }
