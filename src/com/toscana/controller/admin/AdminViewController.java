c1b226408a29813744ceafc028bb417537fc255d
Arandi López <arandilopez.93@gmail.com>
2013-11-28 16:21:34 -0600

    Create admin view controller

---
+++ src/com/toscana/controller/admin/AdminViewController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toscana.controller.admin;
+
+/**
+ *
+ * @author Arandi
+ */
+public class AdminViewController {
+    
+}
