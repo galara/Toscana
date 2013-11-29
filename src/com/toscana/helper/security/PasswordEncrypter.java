e0fb5dac3093a29825fb7047c5d91240cc54f6ae
Arandi López <arandilopez.93@gmail.com>
2013-11-28 19:07:08 -0600

    Update PasswordEncrypter

--- src/com/toscana/helper/security/PasswordEncrypter.java
+++ src/com/toscana/helper/security/PasswordEncrypter.java
@@ -51,4 +51,8 @@ public class PasswordEncrypter {
         }
         return null;
     }
+    
+//    public static void main(String[] args) {
+//        System.out.println("Pass: "+PasswordEncrypter.getEncryptedPassword("administrador"));
+//    }
 }
