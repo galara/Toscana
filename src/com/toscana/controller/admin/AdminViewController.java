28b206d55952534db8f9da40804fedb59f3972e7
Arandi López <arandilopez.93@gmail.com>
2013-11-28 18:12:26 -0600

    Prepare to conect Admin UI with view controller

--- src/com/toscana/controller/admin/AdminViewController.java
+++ src/com/toscana/controller/admin/AdminViewController.java
@@ -4,11 +4,22 @@
  */
 package com.toscana.controller.admin;
 
+import com.toscana.controller.user.UserController;
+
 /**
  *
  * @author Arandi
  */
 public class AdminViewController {
     
+    private UserController userController;
+
+    public AdminViewController() {
+        userController = new UserController();
+    }
+
+    public UserController getUserController() {
+        return userController;
+    }
     
 }
