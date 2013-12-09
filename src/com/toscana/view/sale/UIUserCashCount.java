9e64a106d5c01eb9de31327b80ed7da9f2c763ad
Arandi López <arandilopez.93@gmail.com>
2013-12-09 12:36:55 -0600

    Fix UIUserCashCount name

--- src/com/toscana/view/sale/UIUserCashCount.java
+++ src/com/toscana/view/sale/UIUserCashCount.java
@@ -10,12 +10,12 @@ package com.toscana.view.sale;
  *
  * @author SG_05
  */
-public class UIUseCashCount extends javax.swing.JDialog {
+public class UIUserCashCount extends javax.swing.JDialog {
 
     /**
      * Creates new form UIUseCashCount
      */
-    public UIUseCashCount(java.awt.Frame parent, boolean modal) {
+    public UIUserCashCount(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -117,7 +117,7 @@ public class UIUseCashCount extends javax.swing.JDialog {
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addComponent(textFieldTotal, javax.swing.GroupLayout.Alignment.TRAILING)
                         .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -194,20 +194,20 @@ public class UIUseCashCount extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(UIUseCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UIUserCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(UIUseCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UIUserCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(UIUseCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UIUserCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(UIUseCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UIUserCashCount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                UIUseCashCount dialog = new UIUseCashCount(new javax.swing.JFrame(), true);
+                UIUserCashCount dialog = new UIUserCashCount(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
